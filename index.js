7ccc4bec21b4e4bf0eaf0b9a6cd98325f75b3d07
cor-rine <corrine.is@gmail.com>
2016-08-24 05:37:55 -0700

    Got the scene rendering with the appropriate image

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ var parsePath = require('extract-svg-path').parse;
 var svgMesh3d = require('svg-mesh-3d');
 
 var THREE = require('three');
-var Complex = require('three-simplicial-complex');
+var Complex = require('three-simplicial-complex')(THREE);
 
 
-
-var createScene = function(mesh) {
-  console.log(Complex);
+function createScene(mesh) {
   var complex = Complex(mesh);
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  complex.position.x = 2;
-  scene.add(complex);
-
   var renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
+
+  console.log(complex);
+
+  var mesh = new THREE.Mesh(complex, new THREE.MeshBasicMaterial({ 
+    wireframe: true, 
+    transparent: true,
+    opacity: 1
+  }));
+
+  mesh.position.z = 3.5;
+
+  camera.position.z = 5;
+
+  scene.add(mesh);
+
+  function render() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
+  }
+
+  render();
 }
 
 loadSvg('svg/peg-circle.svg', function (err, svg) {
@@ -29,6 +45,8 @@ loadSvg('svg/peg-circle.svg', function (err, svg) {
     delaunay: false,
     scale: 4
   });
+
+  console.log(mesh);
 
   createScene(mesh);
 });
