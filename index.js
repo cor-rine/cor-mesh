7a2369075159ff51cdaba3eac155d9c92e6be482
Corrine Toracchio <corrine.toracchio@level-studios.com>
2016-08-25 11:33:38 -0700

    My face is loading in webgl

--- index.js
+++ index.js
@@ -5,63 +5,95 @@ var svgMesh3d = require('svg-mesh-3d');
 var THREE = require('three');
 var Complex = require('three-simplicial-complex')(THREE);
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
 
-function createScene(mesh) {
-  var complex = Complex(mesh);
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 900;
+camera.position.x = 100;
+camera.position.y = 500;
 
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-  console.log(complex);
+document.body.appendChild( renderer.domElement );
 
+function drawTriangle() {
   // 412.3,160 461,154.7 459,166.2
   var triangleGeometry = new THREE.Geometry();
   triangleGeometry.vertices.push(new THREE.Vector3(412.3, 160, 0));
   triangleGeometry.vertices.push(new THREE.Vector3(461, 154.7, 0));
   triangleGeometry.vertices.push(new THREE.Vector3(459, 166.2, 0));
-  triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+  triangleGeometry.faces.push(new THREE.Face3(0, 2, 1));
 
   var triangleMesh = new THREE.Mesh(triangleGeometry, new THREE.MeshBasicMaterial({
-    wireframe: true,
     transparent: true,
     opacity: 1,
-    color: 0x2a2a2c
+    side: THREE.DoubleSide,
+    color: 0xffffff
   }));
   triangleMesh.position.set(0, 0.0, 0);
   scene.add(triangleMesh);
-
-  var mesh = new THREE.Mesh(complex, new THREE.MeshBasicMaterial({
-    wireframe: true,
-    transparent: true,
-    opacity: 1
-  }));
-
-  camera.position.z = 300;
-  camera.position.x = 500;
-
-  // scene.add(mesh);
-
-  function render() {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
-  }
-
-  render();
 }
 
-loadSvg('svg/face.svg', function (err, svg) {
+
+function createScene(mesh) {
+
+  // drawTriangle(scene);
+  // var mesh = new THREE.Mesh(complex, new THREE.MeshBasicMaterial({
+  //   // wireframe: true,
+  //   transparent: true,
+  //   side:THREE.DoubleSide,
+  //   opacity: 1,
+  //   color: Math.floor(Math.random()*0xffffff)
+  // }));
+}
+/*
+
+get the SVG as an XML document
+*/
+
+
+loadSvg('svg/face2.svg', function (err, svg) {
   if (err) throw err
 
-  var svgPath = parsePath(svg);
-  var mesh = svgMesh3d(svgPath, {
-    delaunay: false,
-    scale: 4
-  });
 
-  console.log(mesh);
+  var polygons = svg.querySelectorAll('polygon');
+  for(var i =0;i< polygons.length;i++){
+    var poly = polygons[i];
+    var numbers = poly.getAttribute('points')
+                  .split(' ')
+                  .map(function(e){
+                    return parseFloat(e);
+                  });
+    var color = new THREE.Color(poly.getAttribute('fill'));
 
-  createScene(mesh);
+    console.log(numbers);
+
+    var geo = new THREE.Geometry();
+
+      while(numbers.length != 0) {
+        geo.vertices.push(new THREE.Vector3(numbers.shift(), numbers.shift(), 0));
+      }
+      geo.faces.push(new THREE.Face3(0, 2, 1));
+
+    var mesh = new THREE.Mesh(geo, new THREE.MeshBasicMaterial({
+      transparent: true,
+      // wireframe: true,
+      opacity: 1,
+      side:THREE.DoubleSide,
+      color: color
+    }));
+
+    mesh.position.set(0, 0, 0);
+    scene.add(mesh);
+
+    drawTriangle();
+  }
 });
+
+function render() {
+  requestAnimationFrame( render );
+  renderer.render( scene, camera );
+}
+
+render();
