5c1eab48cd69a08ff8c293f65fbe16cb9769fc41
Corrine Toracchio <corrine.toracchio@level-studios.com>
2016-08-26 01:22:17 -0700

    Added face3 and made changes to js

--- index.js
+++ index.js
@@ -5,48 +5,60 @@ var THREE = require('three');
 var Complex = require('three-simplicial-complex')(THREE);
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 
-camera.position.z = 1000;
-camera.position.x = 0;
-camera.position.y = 0;
+camera.position.z = 900;
+camera.position.x = 500;
+camera.position.y = 500;
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
+
+// document.addEventListener('mousedown', function(event) {
+//   console.log(event);
+//   console.log(this);
+// });
+
+document.mousedown = function(event) {
+  console.log(event);
+}
 
 
-loadSvg('svg/face2.svg', function (err, svg) {
+loadSvg('svg/face3.svg', function (err, svg) {
   if (err) throw err
-
 
   var polygons = svg.querySelectorAll('polygon');
   for(var i =0;i< polygons.length;i++){
     var poly = polygons[i];
     var numbers = poly.getAttribute('points')
+                  .replace(/,/g, ' ')
+                  .trim(' ')
                   .split(' ')
                   .map(function(e){
                     return parseFloat(e);
                   });
-    var color = new THREE.Color(poly.getAttribute('fill'));
+    var color = poly.style;
     var geo = new THREE.Geometry();
 
-      while(numbers.length != 0) {
-        geo.vertices.push(new THREE.Vector3(numbers.shift(), numbers.shift(), 0));
-      }
-      geo.faces.push(new THREE.Face3(0, 2, 1));
+    console.log(color);
 
-    var mesh = new THREE.Mesh(geo, new THREE.MeshBasicMaterial({
-      transparent: true,
-      // wireframe: true,
-      opacity: 1,
-      side:THREE.DoubleSide,
-      color: color
-    }));
+    // while(numbers.length != 0) {
+    //   geo.vertices.push(new THREE.Vector3(numbers.pop(), numbers.pop(), 0));
+    // }
+    // geo.faces.push(new THREE.Face3(0, 2, 1));
 
-    mesh.position.set(0, 0, 0);
-    scene.add(mesh);
+    // var mesh = new THREE.Mesh(geo, new THREE.MeshBasicMaterial({
+    //   transparent: true,
+    //   // wireframe: true,
+    //   opacity: 1,
+    //   side:THREE.DoubleSide,
+    //   color: color
+    // }));
+
+    // mesh.position.set(0, 0, 0);
+    // scene.add(mesh);
   }
 });
 
