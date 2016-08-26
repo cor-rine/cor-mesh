1f0978044efe5bb20c91adb30563f34073500cf2
Corrine Toracchio <corrine.toracchio@level-studios.com>
2016-08-26 01:54:31 -0700

    Figured out the face orientation and fixed it

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHei
 var renderer = new THREE.WebGLRenderer();
 
 camera.position.z = 900;
-camera.position.x = 500;
-camera.position.y = 500;
+camera.position.x = -400;
+camera.position.y = -400;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -26,40 +26,35 @@ document.mousedown = function(event) {
 }
 
 
-loadSvg('svg/face3.svg', function (err, svg) {
+loadSvg('svg/face.svg', function (err, svg) {
   if (err) throw err
 
   var polygons = svg.querySelectorAll('polygon');
   for(var i =0;i< polygons.length;i++){
     var poly = polygons[i];
     var numbers = poly.getAttribute('points')
-                  .replace(/,/g, ' ')
-                  .trim(' ')
                   .split(' ')
                   .map(function(e){
                     return parseFloat(e);
                   });
-    console.log(poly);
-    var color = window.getComputedStyle(poly);
+    var color = poly.getAttribute('fill');
     var geo = new THREE.Geometry();
 
-    console.log(color);
+    while(numbers.length != 0) {
+      geo.vertices.push(new THREE.Vector3(-numbers.shift(), -numbers.shift(), 0));
+    }
+    geo.faces.push(new THREE.Face3(0, 2, 1));
 
-    // while(numbers.length != 0) {
-    //   geo.vertices.push(new THREE.Vector3(numbers.pop(), numbers.pop(), 0));
-    // }
-    // geo.faces.push(new THREE.Face3(0, 2, 1));
+    var mesh = new THREE.Mesh(geo, new THREE.MeshBasicMaterial({
+      transparent: true,
+      // wireframe: true,
+      opacity: 1,
+      side:THREE.DoubleSide,
+      color: color
+    }));
 
-    // var mesh = new THREE.Mesh(geo, new THREE.MeshBasicMaterial({
-    //   transparent: true,
-    //   // wireframe: true,
-    //   opacity: 1,
-    //   side:THREE.DoubleSide,
-    //   color: color
-    // }));
-
-    // mesh.position.set(0, 0, 0);
-    // scene.add(mesh);
+    mesh.position.set(0, 0, 0);
+    scene.add(mesh);
   }
 });
 
